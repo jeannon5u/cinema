29bba76fb7221f8566c06ba234a0db07a9af2cc9
jeannon5u <maxime.jeannon9@etu.univ-lorraine.fr>
2020-09-30 17:05:15 +0200

    maj

--- worker.js
+++ worker.js
@@ -1,32 +1,37 @@
-const cachename = static;
+const cacheName = "site-cache";
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(
                 [
-                    'index.js'
+                    '/images/icons/cinema-128x128.png',
+                    '/images/icons/cinema-144x144.png',
+                    '/images/icons/cinema-512x512.png',
+                    'index.html'
                 ]
             );
         })
     );
 });
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
-                    .then(function(response) {
-                        return caches.open(cacheMenu)
-                            .then(function(cache) {
-                                cache.put(event.request, response.clone());
-                                return response;
-                            });
-                    });
-            })
-    )
-});
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request)
+                .then(function(response){
+                    return caches.open(cacheName)
+                        .then(function(cache){
+                            cache.put(event.request, response.clone());
+                            return response;
+                        }
+                    )
+                }
+            );
+        }
+    ));
+}); 
