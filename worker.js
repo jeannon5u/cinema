44cc8f94e6ed2325ff1b9c7b1f2ae46c4e0d1336
jeannon5u <maxime.jeannon9@etu.univ-lorraine.fr>
2020-09-30 17:11:01 +0200

    maj

--- worker.js
+++ worker.js
@@ -1,13 +1,13 @@
-const cacheName = "site-cache";
+const cacheName = "static";
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(
                 [
-                    '/images/icons/cinema-128x128.png',
-                    '/images/icons/cinema-144x144.png',
-                    '/images/icons/cinema-512x512.png',
+                    '/images/icons/icon-128x128.png',
+                    '/images/icons/icon-144x144.png',
+                    '/images/icons/icon-512x512.png',
                     'index.html'
                 ]
             );
